03f20ed06cc0b601b721addf8442161773a7a59e
Davi <davinogueira380@gmail.com>
2026-06-19 19:59:40 -0300

    Parte Davi (java e contato)

---
+++ js/script.js
@@ -0,0 +1,50 @@
+//#region Validação
+//Não mecher (caso queira mecher chamar por Davi)
+//Validação de formúlario
+
+function validarFormulario(){
+    alert("Função fucionando")
+
+    if (document.getElementById('Nome').value.trim() == ""){
+    alert("Digite o seu nome")
+    return false
+    }
+
+     if (document.getElementById('Email').value.trim() == ""){
+        alert("Campo E-mail é obrigatório")
+        return false
+    }else if (validarEmail(document.getElementById('Email').value.trim()) == false){
+        alert("Email está incorreto")
+        return false
+    }
+        if (document.getElementById('Telefone').value.trim() == ""){
+        alert("Campo telefone é obrigatório")
+        return false
+    }else if(document.getElementById('Telefone').value.trim().length !=11){
+        alert("Telefone invalido")
+        return false
+    }
+
+    var opcoes = document.getElementsByName('FormaContato')
+    // return false
+    var qtdSelecionado = 0
+
+    for (let i = 0; i < opcoes.length; i++){
+        if (opcoes[i].checked == true){
+            qtdSelecionado++
+            break
+        }
+    }
+
+     if (qtdSelecionado == 0){
+        alert("Selecione uma forma de contato")
+        return false
+    }
+}
+
+//Validando Email
+function validarEmail(email){
+    return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+}
+
+//#endregion
